3feeef969cac46f5dad4d325fd323bf68358a0ff
hshan <hhsung0120@naver.com>
2020-06-19 00:04:52 +0900

    파일 업로드 수정

--- src/main/java/com/heeseong/util/util/FileUtil.java
+++ src/main/java/com/heeseong/util/util/FileUtil.java
@@ -66,7 +66,7 @@ public class FileUtil {
             file.transferTo(destination);
 
             /*destination.createNewFile();
-            FileOutputStream fos = new FileOutputStream(dest);
+            FileOutputStream fos = new FileOutputStream(destination);
             fos.write(bf.getBytes());
             fos.close();*/
 
