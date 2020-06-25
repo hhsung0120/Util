7b42810445c27274991e27b3955b6daa8092507d
hshan <hhsung0120@naver.com>
2020-06-25 23:15:13 +0900

    DB 유틸 작업중 todo : 1. 자동 insert, 자동 update        2. 컬럼에 해당하는 객체 뽑아주기

---
+++ src/main/java/com/heeseong/util/controller/DatabaseUtilController.java
@@ -0,0 +1,13 @@
+package com.heeseong.util.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/database")
+public class DatabaseUtilController {
+
+}
